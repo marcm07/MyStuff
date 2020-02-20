e587bdec0f1f0cd2f1dc71257eea623a967f63ec
Marc <g18m6731@campus.ru.ac.za>
2020-02-20 15:17:26 +0200

    Problems.Test, problems and sln

--- Problems/Program.cs
+++ Problems/Program.cs
@@ -2,11 +2,31 @@
 
 namespace Problems
 {
-    class Program
+    public class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("String Input: ");
+			string i = Console.ReadLine();
+			Console.WriteLine(Prefix(i));
         }
+
+		public static string Prefix (String input)
+		{
+			if (input.Length == 0)
+				return "null";
+			int len = input.Length;
+			int num_words = 0;
+			for (int k = 0; k<len;k++)
+			{
+				if (input[k] == ' ' && input[k-1] != ' ')
+					num_words++;
+				if (k == (len-1))
+					num_words = num_words+1;
+			}
+			string output = len + "," + num_words + ":" + input;
+			return output;
+		}
+
     }
 }
