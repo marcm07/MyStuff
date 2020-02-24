8026f659def839407bf735756fe85d2e7a61c81d
Marc <g18m6731@campus.ru.ac.za>
2020-02-24 19:30:10 +0200

    Tests Upate

--- Problems.Tests/UnitTest1.cs
+++ Problems.Tests/UnitTest1.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Problems;
 using System;
+using TestCaseData;
 
 namespace Problems.Tests
 {
@@ -22,11 +23,19 @@ namespace Problems.Tests
             string ans2 = Program.Prefix("");
             string ans3 = Program.Prefix("what    ...  did you say?? ");
             //ASSERT
-            Assert.That(ans1, Is.EqualTo("5,1:hello"));
+            Assert.That(ans1, Is.EqualTo("5,1:Hello"));
             Assert.That(ans2, Is.EqualTo("null"));
             Assert.That(ans3, Is.EqualTo("27,5:what    ...  did you say?? "));
         }
-
-
     }
+    /*public class MyDataClass
+    {
+        public static IEnumerable TestCases
+        {
+            get
+            {
+                yield return new TestCaseData("Hello")
+            }
+        }
+    }*/
 }
